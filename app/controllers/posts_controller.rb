3f777ed7a42989dbdf7868bab76fe478ea263500
Mmy26 <yu.b1994masamura@gmail.com>
2021-11-09 19:27:09 +0900

    編集・削除機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!,        except: :index
+  before_action :set_post,                  only: [:show, :update, :edit, :destroy]
+  before_action :contributor_confirmation,  only: [:edit, :update, :destroy]
   def index
     @posts = Post.order("created_at DESC")
   end
@@ -13,14 +15,41 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to root_path
     else
-      render new_post_path
+      render :new
     end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  def update
+    if @post.update(post_params)
+        redirect_to root_path
+    else
+        render :edit
+    end
+  end
+
+  def show
   end
 
   private
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:image, :content, :amount_id, :timing_id).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @post.user
   end
 
 end
