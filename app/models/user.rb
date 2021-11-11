68057ebbc0876884403c31c92f93c602e943bdae
Mmy26 <yu.b1994masamura@gmail.com>
2021-11-11 17:10:53 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :posts
+         has_many :comments
+
   validates :nickname,           presence: true
   validates :weight,             presence: true
   validates :age,                presence: true
